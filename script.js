9f190fe417d16fe10984e32d0a16e46bd4fd3829
Louis Wahl <LouisWahl@LouisWahl.com>
2023-11-21 16:24:21 +0100

    exercise 01

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 export function exercise01(args) {
   const input = args
   const result = []
-
+ 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
-  }
+ if (currentElement === "e") {
+  result.push("")
+ } else if (currentElement === "E") {
+  result.push("")
+ } else {
+  result.push(currentElement)
+ }
+}
   return result.join("")
 }
