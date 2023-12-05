eaede51b25843dc8b330b6f2955eaf591165cf57
Louis Wahl <LouisWahl@LouisWahl.com>
2023-12-05 15:39:42 +0100

    exercise 2 and 3

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ export function exercise01(args) {
 }
   return result.join("")
 }
+
+
+ 
+export function exercise02(args) {
+  return args.toUpperCase ()
+}
+
+export function exercise03(args) {
+  const input = args
+  const result = []
+  let count = 0
+ 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    //Should only include 'e' 
+if (currentElement === 'e'){
+  count = count + 1
+}
+// Should include large 'E' 
+else if (currentElement === 'E'){
+  count = count + 1
+}
+  }
+  return count
+}
